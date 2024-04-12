fd12a94bd4fffd16cd3cc3d5eb83e046b58f06ca
Sora1937 <frostbite1937@outlook.com>
2024-04-12 10:02:19 -0700

    added an html file and a js file

---
+++ Basic_JavaScript_Projects/Project1_Expressions_Alert/JS/main.js
@@ -0,0 +1,11 @@
+// I like variables
+var a = "This is for school 'Hello, World'"
+var b = "Hello, " + "World";
+var Family = "Hedgecokes", Dad = "Gone", Mom = "Sheena", Daughter = "Tayla", Son = "Van", Son2 = "Faebin";
+// End of variables :(
+
+// Function for changing text on a button when clicked
+function My_First_Function() {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
