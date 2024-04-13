5da6b836e23a6b67089b3c6e7405c203ef221563
Sora1937 <frostbite1937@outlook.com>
2024-04-13 10:17:29 -0700

    Added a new html and js file

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -3,3 +3,13 @@ function myFunction() {
     sentence += " quite a lot from this bootcamp!";
     document.getElementById("Concatenate").innerHTML = sentence;
 }
+
+function Multiplytwo(x, y) {
+    var product = x * y;
+    document.getElementById("Product").innerHTML = product;
+}
+
+function addition_Function() {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+}
