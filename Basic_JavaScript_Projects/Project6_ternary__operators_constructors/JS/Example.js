fcf5af27578dc5df97d566b4e6048bc2916859b4
Sora1937 <frostbite1937@outlook.com>
2024-04-17 11:32:57 -0700

    changes a js file and removes a js file

--- Basic_JavaScript_Projects/Project6_ternary__operators_constructors/JS/Example.js
+++
@@ -1,9 +0,0 @@
-function count_Function() {
-    document.getElementById("Counting").innerHTML = Count()
-    function Count() {
-        var Starting_point = 9;
-        function Plus_one() {Starting_point += 1}
-        Plus_one();
-        return Starting_point;
-    }
-}
